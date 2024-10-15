b1380830542ba45f2d23bddc03823c269905fec1
Phil Belanger <philb@bitheads.com>
2024-10-15 09:06:38 -0400

    Create one_way_match_test.dart

---
+++ test/one_way_match_test.dart
@@ -0,0 +1,48 @@
+import 'package:braincloud_dart/braincloud_dart.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'utils/test_base.dart';
+
+void main() {
+  BCTest bcTest = BCTest();
+
+  setUpAll(bcTest.setupBC);
+
+  group("Test One Way Match", () {
+    var streamId;
+
+    test("startMatch()", () async {
+      ServerResponse response = await bcTest.bcWrapper.oneWayMatchService
+          .startMatch(otherPlayerId: userB.profileId!, rangeDelta: 1000);
+
+      streamId = response.data?["playbackStreamId"];
+      expect(response.statusCode, StatusCodes.ok);
+    });
+
+    test("cancelMatch()", () async {
+      ServerResponse response = await bcTest.bcWrapper.oneWayMatchService
+          .cancelMatch(playbackStreamId: streamId);
+
+      expect(response.statusCode, StatusCodes.ok);
+    });
+
+    test("startMatch()", () async {
+      ServerResponse response = await bcTest.bcWrapper.oneWayMatchService
+          .startMatch(otherPlayerId: userB.profileId!, rangeDelta: 1000);
+
+      streamId = response.data?["playbackStreamId"];
+      expect(response.statusCode, StatusCodes.ok);
+    });
+
+    test("completeMatch()", () async {
+      ServerResponse response = await bcTest.bcWrapper.oneWayMatchService
+          .completeMatch(playbackStreamId: streamId);
+      expect(response.statusCode, StatusCodes.ok);
+    });
+
+    /// END TEST
+    tearDownAll(() {
+      bcTest.dispose();
+    });
+  });
+}
