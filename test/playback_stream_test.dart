c80523915257aebfec60f1e6b8ca86eed816b017
Mario Couture <MarioC@bitheads.com>
2024-11-08 15:41:45 -0500

    Add missing Test cases for PlabackStream

--- test/playback_stream_test.dart
+++ test/playback_stream_test.dart
@@ -36,6 +36,36 @@ void main() {
       expect(response.statusCode, StatusCodes.ok);
     });
 
+    test("getRecentStreamsForInitiatingPlayer()", () async {
+      ServerResponse response = await bcTest.bcWrapper.playbackStreamService
+          .getRecentStreamsForInitiatingPlayer(maxNumStreams: 1);
+
+      expect(response.statusCode, StatusCodes.ok);
+      expect(response.data?['streams'], isA<List>());
+    });
+
+    test("getRecentStreamsForInitiatingPlayer(w/playerId)", () async {
+      ServerResponse response = await bcTest.bcWrapper.playbackStreamService
+          .getRecentStreamsForInitiatingPlayer(initiatingPlayerId:userB.profileId,maxNumStreams : 1);
+
+      expect(response.statusCode, StatusCodes.ok);
+      expect(response.data?['streams'], isA<List>());
+    });
+
+    test("getRecentStreamsForTargetPlayer()", () async {
+      ServerResponse response = await bcTest.bcWrapper.playbackStreamService
+          .getRecentStreamsForTargetPlayer(maxNumStreams: 1);
+      expect(response.statusCode, StatusCodes.ok);      
+      expect(response.data?['streams'], isA<List>());      
+    });
+
+    test("getRecentStreamsForTargetPlayer(w/playerId)", () async {
+      ServerResponse response = await bcTest.bcWrapper.playbackStreamService
+          .getRecentStreamsForTargetPlayer(targetPlayerId: userB.profileId,maxNumStreams: 1);
+      expect(response.statusCode, StatusCodes.ok);      
+      expect(response.data?['streams'], isA<List>());      
+    });
+
     test("endStream()", () async {
       ServerResponse response = await bcTest.bcWrapper.playbackStreamService
           .endStream(playbackStreamId: streamId);
