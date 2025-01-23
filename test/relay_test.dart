facf222b40c92cdba9de0763f09e7f0a58d98e7e
Mario Couture <MarioC@bitheads.com>
2025-01-23 14:24:10 -0500

    Make Windows retry Relay UDP test
    only windows seem to have trouble with that one.

--- test/relay_test.dart
+++ test/relay_test.dart
@@ -323,6 +323,7 @@ print(" converted bytes is $reloadedMask");
       'browser': [Skip('Browser does not support Relay UDP connection, skipping')]
     });
 
+    /// This test has a retry to ensure that networking issues are not the cause of the failure.
     test("FullFlow UDP", () async {
       roomAssignedDetectedCompleter = null;
       if (bcTest.bcWrapper.rttService.isRTTEnabled())  await disconnectRelay();
@@ -332,7 +333,8 @@ print(" converted bytes is $reloadedMask");
       expect(successCount, 4);
       expect(failureCount, 0);
     }, timeout: Timeout.parse("90s"),onPlatform: {
-      'browser': [Skip('Browser does not support Relay UDP connection, skipping')]
+      'browser': [Skip('Browser does not support Relay UDP connection, skipping')],
+      'windows': [Retry(2)]  //Make windows retry once more for this test.
     });
 
     test("FullFlow WebSocket", () async {
